42af442217b291ae8bfc0f77ac921f028efc382a
TzuYangDaniel Lin <lintzu@flip1.engr.oregonstate.edu>
2014-03-05 09:17:36 -0800

    Improved Correlation Program with better variable names and better comments

---
+++ Correlation_Version_1_Improved/Correlation_Tool_Core_Functions/Analysis.m
@@ -0,0 +1,43 @@
+% function corr_res = Analysis(theta,Brange,Sigma)
+% Description:
+%       
+%    The function calls the correlation script, performs the correlation
+%    analysis and returns the result 
+%
+% Fields:
+%
+%      theta: Angle range for image 
+%      Brange: the translational range for the image 
+%      Sigma: The correlation factor 
+%
+% Initial conditions: 
+%      Theta and Brange must be numeric integers, no control characters, and are  
+%      in the form "<start>:<step>:<end>. Sigma can be a decimal value. 
+%
+% Final conditions: 
+%      Returns the correlation analysis map.
+%
+%
+
+function corr_res = Analysis(theta,Brange,Sigma)
+    
+    global image_read;
+    
+     if isempty(image_read)
+    
+        errordlg('You did not select an image');
+        corr_res = 0;
+        return;
+    
+    end
+    
+    I = rgb2gray(image_read);
+    image = single(I);
+    
+    thet = [theta(1),theta(2),theta(3)];
+    Brang = [Brange(1),Brange(2),Brange(3)];
+   
+    corr = correlation_line(image, thet, Brang, Sigma);
+    disp('Processing image complete');
+    corr_res = corr; 
+end
---
+++ Correlation_Version_1_Improved/Correlation_Tool_GUI_Specific_functions/getterandchecker.m
@@ -0,0 +1,69 @@
+% function [angle,B,Sig, status] = getterandchecker(handle,status)
+%
+% Description:
+%       
+%    This function gets the theta, Brange, and Sigma values from the
+%    listbox. Then, it checks whether those values are valid or not. If
+%    they are, the program returns them and sets the status to 0. 
+%
+%
+% Fields:
+%
+%    handle: Graphical Handle 
+%    status: status to be returned
+%
+% Initial conditions: 
+%    Handle must be a GUI handle, status must be a type int. 
+%
+% Final conditions: 
+%
+%    Returns the sanitized theta, Brange, Sigma for the correlation 
+%    analysis and status = 0 on success. If failure, the status is set to 1 
+%    and the return parameters are set to empty strings. 
+%     
+function [angle,B,Sig, status] = getterandchecker(handle,status)
+  
+    angle_temp = get(handle.edit2,'string');
+    B_temp = get(handle.edit3,'string');
+    Sig_temp = get(handle.edit4, 'string');
+        
+    Sig_temp = str2num(Sig_temp);
+   
+    if isempty(angle_temp) || isempty(Sig_temp) || isempty(B_temp)
+  
+        errordlg('One of your fields for theta, sigma, brange is empty or you did not select an image');
+        angle = '';
+        B = '';
+        Sig = '';
+        status = 1;
+        return;
+           
+    end 
+    
+    % Check theta and Brange input 
+    [theta,Bran] = sanitize(angle_temp,B_temp);
+   
+    % If the return values for sanitize are empty, something went wrong
+    if strcmp(theta,'') == 1|| strcmp(Bran,'') == 1
+      
+        angle = '';
+        B = '';
+        Sig = '';
+        status = 1;
+        return;
+        
+    end
+    
+     if isscalar(Sig_temp) == 0
+        
+        errordlg('Your sigma value is not a number, please re-enter');
+        status = 1;
+        return;
+        
+     end
+       
+    angle = theta;
+    B = Bran;        
+    Sig = Sig_temp;
+    status = 0;
+end
