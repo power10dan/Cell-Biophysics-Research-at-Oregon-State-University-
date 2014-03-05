1738ccaa37e5a5a1839fe6661de7e87dad866a33
TzuYangDaniel Lin <lintzu@flip1.engr.oregonstate.edu>
2014-03-05 10:06:21 -0800

    This version has the citations on where I got some of my code from

---
+++ Correlation_Version_1_Improved_beta/Correlation_Tool_GUI_Specific_functions/prompt.m
@@ -0,0 +1,43 @@
+% function answer = prompt   
+% 
+% Description:
+%       
+%   This function prompts the user to enter the name of the destination
+%   folder in which the user wants to save his or her files. Code is based
+%   on this Matlab documentation: http://www.mathworks.com/help/matlab/ref/inputdlg.html
+%
+% Fields:None
+%
+%   
+% Initial conditions: None
+%     
+%
+% Final conditions: 
+%
+%     Returns the name of the destination folder
+%
+
+function answer = prompt   
+
+% prompts the user for a folder name 
+    prompt = {'Enter the name of your folder you want your image to be saved in  if it is a current folder, type the name of the folder.'};
+    dlg_title = 'Input';
+    num_lines = 1;
+    answer_temp = inputdlg(prompt,dlg_title,num_lines); % store into temp variable for checking purposes
+    
+    if isempty(answer_temp)== 1 % user hit cancel button 
+        answer = '';
+        return;
+        
+    end
+    
+    if isempty(answer_temp{1})
+    
+        errordlg('You forgot to enter something in your dialog');
+        answer = '';
+        return;
+        
+    end
+            
+    answer = answer_temp;
+end
