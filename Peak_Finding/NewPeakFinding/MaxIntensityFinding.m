984d64aac5eca65f3ff5e2d2decc508ecd3d6604
power10dan <lintzu@onid.oregonstate.edu>
2014-07-25 13:29:26 -0700

    New Version of Peak Finding with Professor Sun's Help and Bench Mark Testing For The New Function"

--- Peak_Finding/NewPeakFinding/MaxIntensityFinding.m
+++ Peak_Finding/NewPeakFinding/MaxIntensityFinding.m
@@ -1,9 +1,12 @@
 % function MaxIntensityFinding (correlation_map)
+% 
+% Modifications of program other than modified by me: 
+% Bo Sun, 7/25/14
 %
 % Description: 
 % This function finds the maximum values inside a Correlation
-% Map and generates an intensity map with markers plotted on the map to
-% locate the maximums on the correlation map. 
+% Map and generate an intensity map with markers plotted on the map to
+% locate  the maximums on the correlation map. 
 %
 % Fields: correlation_map 
 %
@@ -12,42 +15,39 @@
 %
 % Post-Condition: an intensity map showing where the maximums are on the
 % correlation map. 
-%
 
-function MaxIntensityFinding (correlation_map)
+function [label_map] = MaxIntensityFinding (correlation_map,pars)
     
-    figure, imagesc(correlation_map);
-    correlation_map(364.
-    
-    distance_threshold = 10;  % dummy value, subject to change
+    if(isfield(pars,'distance_threshold'))
         
-    size_corr = size(correlation_map);
+        distance_threshold = pars.distance_threshold ;
     
-    % create an intensity map grid plot
-    intensity_map = zeros(size_corr(1), size_corr(2));
-    figure, imagesc(intensity_map);  
-         
-    for idx = 1:10:size_corr(2)
-
-        if idx + distance_threshold < size_corr(2)
-            % parse out 1st 10 rows of matrix
-            initial_comparison_matrix = correlation_map(:,idx:idx+distance_threshold);
-      
-            % find maximum values in every single column of the initial matrix
-            local_max_values = max(initial_comparison_matrix);
-            maximum_pixel = max(local_max_values)
-            
-            % index of maximum pixel
-            index_ref_pixel = find(correlation_map == maximum_pixel)
-            [xcord, ycord] = ind2sub(size(correlation_map), index_ref_pixel)
-            
-            hold on;
-            plot(xcord, ycord,'x', 'MarkerSize', 10, 'MarkerFaceColor', 'k');
-            hold off;
+    else
         
-        end 
-            
+        distance_threshold = 10;  % dummy value, subject to change
     end
+    
+    size_corr = size(correlation_map);
  
+    label_map = zeros(size_corr);
+    
+    [xgrid,ygrid] = meshgrid(1:size_corr(2),1:size_corr(1));
+    
+    for indx = 1:size_corr(2)
+        
+        for indy = 1:size_corr(1)
+            
+            dist = sqrt((xgrid-indx).^2 + (ygrid-indy).^2);
+            pixuseful = dist < distance_threshold;
+            
+            if (correlation_map(indy,indx) >= max(correlation_map(pixuseful)))
+               
+                label_map(indy,indx) = 1;
+            
+            end
+           
+        end
+    end
+
 end
 
