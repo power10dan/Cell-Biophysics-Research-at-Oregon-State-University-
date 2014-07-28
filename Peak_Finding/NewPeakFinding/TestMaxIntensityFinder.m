afb0927a4b686c0d7332f7588e5d54c5146a1e58
power10dan <lintzu@onid.oregonstate.edu>
2014-07-28 11:52:47 -0700

    Almost Bug Free Testing Function

--- Peak_Finding/NewPeakFinding/TestMaxIntensityFinder.m
+++ Peak_Finding/NewPeakFinding/TestMaxIntensityFinder.m
@@ -1,34 +1,44 @@
+% function TestMaxIntensityFinder
+% 
+% Description: 
+% Testing function for MaxIntensityFinding.m
+%
+% Fields: none
+%
+% Pre-Condition: none
+%
+% Post-Condition: none
+%
+
 function TestMaxIntensityFinder
 
     image_test = zeros(120,100);
     
     % at random coordinates in each column of matrix, place noise intensities 
     
-    sz_image_blank = size(image_test);
+    sz_image_test = size(image_test);
  
-    for idx = 1: sz_image_blank(2)
-        
-        rand_position = randi(120,1,30);
+    for idx = 1: sz_image_test(2)
+
+        rand_positions = randi(120,1,30);     
         col = image_test(:,idx);
         
-        
-        for idx2 = 1:numel(rand_position)
+        for idx2 = 1:numel(rand_positions)
             
            rand_color = randi(255,1,1);
-           rand_position
-           rand_position(idx2)
-           return;
-           col(rand_position(idx2)) = rand_color;                          
-          
+           col(rand_positions(idx2)) = rand_color; 
+            
         end
- 
+        
+        image_test(:,idx) = col;
+        
     end
-
-    imagesc(image_test);
-
-
-
-
-
+    
+    MaxIntensityFinding(image_test,pars);
+    
+    
+    
+    
+    
 
 end
