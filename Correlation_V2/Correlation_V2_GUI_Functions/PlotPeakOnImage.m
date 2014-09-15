3cafd68c3947cc6a15c27848bb453d876f2d9bab
power10dan <lintzu@onid.oregonstate.edu>
2014-09-15 13:26:04 -0700

    Initial Attempt To Get Line Data From Angle And Translation

--- Correlation_V2/Correlation_V2_GUI_Functions/PlotPeakOnImage.m
+++ Correlation_V2/Correlation_V2_GUI_Functions/PlotPeakOnImage.m
@@ -15,11 +15,41 @@ function PlotPeakOnImage(handles)
     peak_image = getimage(handles.axes11);
     original_image = getimage(handles.axes10);
     % change peak map matrix indices to Cartesian coordinates
-    [xcord,ycord] = ind2sub(size(peak_image), find(peak_image == 1));
-    % plot the peaks onto the original image
-	axes(handles.axes10);
-	for idx = 1:numel([xcord,ycord])
-		plot(xcord,ycord,'r+');   
-    end     
+    [angle,trans_range] = ind2sub(size(peak_image), find(peak_image == 1));
+    % use angle and trans_range to generate line and plot on the original
+    % image
+    for idx = 1:numel([angle, trans_range])       
+        [x0, y0] = DetermineOrigin;
+        x1 = x0 +  trans_range(idx) * cos(angle(idx));
+        y1 = y0 +  trans_range(idx) * sin(angle(idx));
+        
+        
+    
+    
+    end
+    
+ 
+    
+%     % plot the peaks onto the original image
+% 	axes(handles.axes10);
+% 	hold on;
+% 	for idx = 1:numel([xcord,ycord])
+% 		plot(xcord,ycord,'r+','markers',12,'LineWidth',2,'MarkerEdgeColor','k');   
+%     end  
+%     hold off;
  end
 
+ function [xorigin,yorigin] = DetermineOrigin
+ 
+    
+ 
+    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ end
