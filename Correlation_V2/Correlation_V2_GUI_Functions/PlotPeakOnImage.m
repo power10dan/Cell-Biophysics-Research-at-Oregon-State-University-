91f6cb02a5c14e0ca67a976c2491e51641e0b303
power10dan <lintzu@onid.oregonstate.edu>
2014-09-19 13:35:11 -0700

    Modified Function To Generate Lines But Plotting Over Original Image Is Still In Progress

--- Correlation_V2/Correlation_V2_GUI_Functions/PlotPeakOnImage.m
+++ Correlation_V2/Correlation_V2_GUI_Functions/PlotPeakOnImage.m
@@ -14,20 +14,19 @@
 function PlotPeakOnImage(handles)
     peak_image = getimage(handles.axes11);
     original_image = getimage(handles.axes10);
+    sz_original_img = size(original_image);
     % change peak map matrix indices to Cartesian coordinates
-    [trans_range, angle] = ind2sub(size(peak_image), find(peak_image == 1))
+    [angle, trans_range] = ind2sub(size(peak_image), find(peak_image == 1));
     % convert image to grayscale for better visualization of plotted line 
-    axes(handles.axes6);
+    axes(handles.axes10);
     % use angle and trans_range to generate line and plot on the original
     % image    
-    for idx = 1:numel([angle, trans_range]) 
-% 		% set origin of the line to be zero 
-%         x_cord = trans_range(idx) * cosd(angle(idx));
-%         y_cord = trans_range(idx) * sind(angle(idx));	
-		% plot the peaks onto the original image
-		hold on;
-		plot(angle,trans_range,'r+','markers',12,'LineWidth',2,'MarkerEdgeColor','k');   
-	    hold off;   
+    for idx = 1:numel(angle) 
+        line_img = fuzzyline_gen(sz_original_img(1), sz_original_img(2),...
+                                 angle(idx), trans_range(idx), ...
+                                 str2num(get(handles.edit2, 'String')));       
+        origin_image_fused = imfuse(line_img, original_image);
+        imagesc(origin_image_fused);
     end
  end
 
