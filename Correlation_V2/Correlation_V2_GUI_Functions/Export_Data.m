b88a867d714b70dc74d0f2a10399b696012a6295
power10dan <lintzu@onid.oregonstate.edu>
2015-01-06 15:45:39 -0800

    Export Can Now Save Two Diff Files

--- Correlation_V2/Correlation_V2_GUI_Functions/Export_Data.m
+++ Correlation_V2/Correlation_V2_GUI_Functions/Export_Data.m
@@ -1,8 +1,17 @@
-function Export_Data(data_to_be_saved, type)
-   if strcmp(type, 'Input Arguments') == 0
-        save Input_Arguments data_to_be_saved;
-   else
-        save Additional_Parameters data_to_be_saved
-   end
+function Export_Data(saved_data, type, mode)
+
+    if strcmp(mode, 'Sub-window-Analysis') == 1
+        if strcmp(type, 'Input Arguments') == 1
+            save ('Input_Arguments_Subwindow','saved_data');
+        else
+            save('Additional_Parameters_Subwindow', 'saved_data');
+        end
+    else
+        if strcmp(type, 'Input Arguments') == 1
+            save('Input_Arguments_Regular_Corr', 'saved_data');
+        else
+            save('Additional_Parameters_Regular_Corr', 'saved_data');
+        end
+    end    
 end
 
