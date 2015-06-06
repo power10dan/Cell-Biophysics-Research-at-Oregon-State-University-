ec6e054f4790a0c616d5b0409d9b8e386b1b0ad8
power10dan <lintzu@onid.oregonstate.edu>
2015-06-06 14:47:06 -0700

    Removed Useless File

--- Correlation_V2_Core_Function/run_test_noise_image.m
+++
@@ -1,9 +0,0 @@
-function run_test_noise_image(num_file)
-    disp('Noise: Gaussian')
-%     test_noise_graph('gaussian', num_file)
-    disp('Noise: Speckle')
-    test_noise_graph('speckle', num_file)
-    disp('Noise: Salt & Pepper');
-%     test_noise_graph('salt & pepper', num_file)
-    
-end
